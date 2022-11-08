2071611fc6cb8b1628985be20ac6b11f459a6fe9
Lars Holmberg <lars.holmberg@svenskaspel.se>
2022-11-08 17:59:22 +0100

    add example of parametrization

--- tests/plugin_example.py
+++ tests/plugin_example.py
@@ -1,12 +1,25 @@
 from har2locust.plugin import entriesprocessor, valuesprocessor, cstprocessor
 import logging
 import libcst as cst
+import re
 
 
 @entriesprocessor
 def log_something_and_drop_everthing_but_the_first_request(entries):
     logging.info(f"hello")
-    entries[:] = [entries[0]]
+    entries[:] = [entries[0]]  # update list in-place
+
+
+@entriesprocessor
+def parametrize_ssn(entries):
+    # this assumes you have a dict on your user called self.customer
+    for e in entries:
+        if "postData" in e["request"]:
+            e["request"]["postData"]["text"] = re.sub(
+                r'"personalId":"\d*"',
+                "\"personalId\":self.customer['ssn']",
+                e["request"]["postData"]["text"],
+            )
 
 
 @valuesprocessor
