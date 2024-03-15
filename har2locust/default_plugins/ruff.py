554167192b5bb49e87995122f741402c7a09fc2a
Lars Holmberg <Lars.Holmberg@svenskaspel.se>
2024-03-15 09:33:29 +0100

    use --exit-zero instead of just ignoring ruff check exit code

--- har2locust/default_plugins/ruff.py
+++ har2locust/default_plugins/ruff.py
@@ -13,15 +13,24 @@ def ruff(py: str):
         not p.returncode
     ), f"Ruff failed to format the output - perhaps your template is broken? unformatted output was: {py}"
     p = subprocess.Popen(
-        ["ruff", "check", "--fix", "-q", "-"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True
+        [
+            "ruff",
+            "check",
+            "--fix",
+            "-q",
+            "--exit-zero",  # ignore linting fails, we're most interested in applying fixes, and it is up to the user to examin errors
+            "-",
+        ],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        text=True,
     )
     assert p.stdin  # keep linter happy
     p.stdin.write(stdout)
     stdout, stderr = p.communicate()
-    # whatever.
-    # assert (
-    #     not p.returncode
-    # ), f"Ruff failed to check/fix the output - perhaps your template is broken? unformatted output was: {py}"
+    assert (
+        not p.returncode
+    ), f"Ruff failed to check/fix the output - perhaps your template is broken? unformatted output was: {stdout}"
 
     # for some reason the subprocess returns an extra newline, get rid of that
     return stdout[:-1]
